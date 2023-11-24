94358b2cb6e67c1a4e7f7956048a8d32903e3eb3
qweliant <dmtorcode@gmail.com>
2023-11-24 04:24:27 -0500

    TODO: get dependencies working

---
+++ note/app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var editorjs_preconfigured_1 = require("editorjs-preconfigured");
+var ed = new editorjs_preconfigured_1.BlockEditor({ editorData: {} });
+console.log("here");
--- note/app.ts
+++ note/app.ts
@@ -1,2 +1,5 @@
-import {BlockEditor} from "editorjs-preconfigured"
+import { BlockEditor } from "editorjs-preconfigured";
 
+const ed = new BlockEditor({ editorData: {} });
+
+console.log("here");
