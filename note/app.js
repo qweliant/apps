52a62afa793e7ac74e481ae449e6b18d5a83bfa1
qweliant <dmtorcode@gmail.com>
2023-11-26 15:33:29 -0500

    add everything

--- note/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var editorjs_preconfigured_1 = require("editorjs-preconfigured");
-var ed = new editorjs_preconfigured_1.BlockEditor({ editorData: {} });
-console.log("here");
--- note/app.ts
+++
@@ -1,5 +0,0 @@
-import { BlockEditor } from "editorjs-preconfigured";
-
-const ed = new BlockEditor({ editorData: {} });
-
-console.log("here");
