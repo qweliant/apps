26912d616ac1e5af8d86b97affb975e657d15f26
qweliant <dmtorcode@tutanota.com>
2025-01-07 15:12:08 -0500

    Added liha as a submodule, rmoved svelte note app, replace with flutter note app, added blog. TODO: create 2b that can export markdown to my blogs directory

--- stn-ocn/.netlify/server/entries/pages/strong/_page.svelte.js
+++
@@ -1,7 +0,0 @@
-import { c as create_ssr_component } from "../../../chunks/ssr.js";
-const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<section data-svelte-h="svelte-13aene0"><h1>Strong ass clone</h1></section>`;
-});
-export {
-  Page as default
-};
--- stn-ocn/src/store.ts
+++
@@ -1,4 +0,0 @@
-import { writable } from 'svelte/store';
-import type EditorJS from '@editorjs/editorjs';
-
-export const editorStore = writable({} as EditorJS);
