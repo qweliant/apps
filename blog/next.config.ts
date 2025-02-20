3697f2bda04ee4a674da533062849987b673e051
qweliant <dmtorcode@tutanota.com>
2025-02-20 13:05:58 -0500

    hmmm, am i doing this wrong, well in the not so clearly defined next way

--- blog/next.config.ts
+++ blog/next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 import withMDX from "@next/mdx";
+// import rehypeAutolinkHeadings from "rehype-autolink-headings";
+// import rehypeSlug from "rehype-slug";
 
 const mdx = withMDX({
   extension: /\.mdx?$/,
+  // options: {
+  //   rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
+  // },
 });
 const nextConfig: NextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"], // Enable MDX alongside TS/TSX
